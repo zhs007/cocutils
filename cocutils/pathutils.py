4be83a1c490f5cde1f95a81020cac061d0222974
zhs007 <zerrozhao@gmail.com>
2021-11-11 09:00:59 +0000

    upd

--- cocutils/pathutils.py
+++ cocutils/pathutils.py
@@ -5,7 +5,7 @@ import os
 def findAllFile(basePath, endsWith):
     for root, ds, fs in os.walk(basePath):
         for f in fs:
-            if f.endswith(endsWith):
+            if not f.startswith('.') and f.endswith(endsWith):
                 fullname = os.path.join(root, f)
                 yield fullname
 
