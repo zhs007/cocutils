ca8fa1e21570793dfd732143cfc577accfa3a8da
ZerroZhao <zerrozhao@gmail.com>
2021-11-11 21:20:25 +0800

    updated docker

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,4 @@
 docker stop cocutils
 docker rm cocutils
 
-docker run -d --name cocutils -v $PWD/output:/src/cocutils/output cocutils
+docker run -d --name cocutils -v $PWD/output:/src/cocutils/output -v $PWD/coccsv:/src/cocutils/coccsv -v $PWD/cocsc:/src/cocutils/cocsc cocutils
